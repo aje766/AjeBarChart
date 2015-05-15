92d474d1190e11a7df129a9218c4004f08039325
Aurionpro2 <aurionpro02@aurionpro02s-Mac-mini-2.local>
2015-05-15 14:01:40 +0530

    Initial Commit

---
+++ MyBarChartC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyBarChartC
+//
+//  Created by Aurionpro2 on 15/05/15.
+//  Copyright (c) 2015 Aurionpro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
